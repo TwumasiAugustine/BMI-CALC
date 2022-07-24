4b4b07049acf632ba80872b451fc573c0e11eb5b
TwumasiAugustine <95767645+TwumasiAugustine@users.noreply.github.com>
2022-07-24 10:54:53 +0000

    Functional changes

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 var weight = document.getElementById("weight-el");
 var height = document.getElementById("height-el");
-var result= document.getElementById("result-el");
+const result = document.getElementById("result-el");
 var message = document.getElementById("message-el");
-var error = document.getElementById("error-el");
 const btn = document.getElementById("btn");
 
-function stopTyping() {
+function onTyping() {
     weight.type = "number";
     height.type = "number";
     if(weight.value.length > 0 && height.value.length > 0) {
@@ -16,12 +15,14 @@ function stopTyping() {
         btn.style.cursor = "not-allowed";
     }
 }
+onTyping();
 
-function bmiResults() {
+btn.addEventListener("click", function() {
     let weightValue = weight.value;
     let heightValue = height.value;
     let convert = 10000;
-
+    weight.value = ""
+    height.value = ""
     let bmi = weightValue / heightValue**2 * convert;    
     result.textContent ="BMI: " + bmi.toFixed(2);
     if (bmi < 18.5) {
@@ -30,11 +31,13 @@ function bmiResults() {
     }else if(bmi >= 18.5 && bmi < 24.9) {
         message.style.color = "green";
         message.textContent = "You have Normal weight!";
-    }else if (bmi >= 25 && bmi < 29.9 ){
+    }else if (bmi > 24.9 && bmi < 29.9 ){
         message.style.color = "rgba(235, 45, 45, 0.756)";
         message.textContent =  "You are Overweight!";
-    }else if(bmi >= 30){
+    }else if(bmi > 29.9){
         message.style.color = "red";
         message.textContent = "You are Obese!";
     } 
-} 
+    document.getElementById("btn").disabled = true;
+    btn.style.cursor = "not-allowed";
+})
